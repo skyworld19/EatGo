abe3138ef3de0f0ff1f87c75db818193823432cf
semin <cing9393@gmail.com>
2020-11-03 01:23:11 +0900

    가짜객체 사용

--- eatgo-api/src/test/java/kr/co/study/eatgo/interfaces/RestaurantControllerTest.java
+++ eatgo-api/src/test/java/kr/co/study/eatgo/interfaces/RestaurantControllerTest.java
@@ -1,19 +1,21 @@
 package kr.co.study.eatgo.interfaces;
 
 import kr.co.study.eatgo.application.RestaurantService;
-import kr.co.study.eatgo.domain.MenuItemRepository;
-import kr.co.study.eatgo.domain.MenuItemRepositoryImpl;
-import kr.co.study.eatgo.domain.RestaurantRepository;
-import kr.co.study.eatgo.domain.RestaurantRepositoryImpl;
+import kr.co.study.eatgo.domain.MenuItem;
+import kr.co.study.eatgo.domain.Restaurant;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,17 +27,25 @@ public class RestaurantControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @SpyBean(RestaurantService.class)
+    @MockBean
     private RestaurantService restaurantService;
 
-    @SpyBean(RestaurantRepositoryImpl.class)
-    private RestaurantRepository restaurantRepository;
-
-    @SpyBean(MenuItemRepositoryImpl.class)
-    private MenuItemRepository menuItemRepository;
+//    @SpyBean(RestaurantService.class)
+//    private RestaurantService restaurantService;
+//
+//    @SpyBean(RestaurantRepositoryImpl.class)
+//    private RestaurantRepository restaurantRepository;
+//
+//    @SpyBean(MenuItemRepositoryImpl.class)
+//    private MenuItemRepository menuItemRepository;
 
     @Test
     public void list() throws Exception {
+        List<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
+
+        given(restaurantService.getRestaurants()).willReturn(restaurants);
+
         mvc.perform(get("/restaurants"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
@@ -47,6 +57,14 @@ public class RestaurantControllerTest {
 
     @Test
     public void detail() throws Exception {
+        Restaurant restaurant1 = new Restaurant(1004L, "Bob zip", "Seoul");
+        restaurant1.addMenuItem(new MenuItem("Kimchi"));
+
+        Restaurant restaurant2 = new Restaurant(2020L, "Cyber Food", "Seoul");
+
+        given(restaurantService.getRestaurant(1004L)).willReturn(restaurant1);
+        given(restaurantService.getRestaurant(2020L)).willReturn(restaurant2);
+
         mvc.perform(get("/restaurants/1004"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
